656a71460fb038c25936de598b1da4be24ba0b73
itsmushi <jmushi175@gmail.com>
2022-07-20 11:19:54 +0300

    vite update on main page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Live updates on push</p>
+        <p>Live updates on push, with Vite!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
